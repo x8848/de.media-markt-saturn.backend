82c3b1786bf79493d9fef0abd6167e0452b11147
Dima <dmytro@keleya.de>
2022-10-15 16:28:02 -0600

    change port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PORT } from './utils/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,5 @@
-export const MONGODB_URL =
-  process.env.MONGODB_URL || 'mongodb://localhost/nest';
+const LOCAL_PORT = 4000;
+const LOCAL_MONGODB_URL = 'mongodb://localhost/nest';
+
+export const PORT = process.env.PORT || LOCAL_PORT;
+export const MONGODB_URL = process.env.MONGODB_URL || LOCAL_MONGODB_URL;
